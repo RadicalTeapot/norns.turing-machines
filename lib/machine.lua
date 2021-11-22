cc2d22a2a03a3c5c249962ca8b7f9727319198de
Mathias Capdet <mathias.capdet@tescan.com>
2021-11-22 13:59:14 +0100

    Use class for sequences

---
+++ lib/machine.lua
@@ -0,0 +1,147 @@
+ui = require 'ui'
+
+local Machine = {}
+Machine.__index = Machine
+
+function Machine.new(id, label, sequence_length, running)
+    local s = setmetatable({}, Machine)
+    s.id = id or 'machine'
+    s.label = label or id or 'Machine'
+
+    s.sequence_length = sequence_length or 16
+    s:init_sequence()
+    s.position = 1
+    s.running = running or true
+    s.active = true
+    s.previous = nil
+    s.next = nil
+
+    s.dials = {
+        steps=ui.Dial.new(55, 28, 22, 8, 1, s.sequence_length, 1, 1, {s.sequence_length * 0.5}),
+        knob=ui.Dial.new(100, 28, 22, 50, 0, 100, 1, 50, {50})
+    }
+
+    return s
+end
+
+function Machine:add_params(sequence_controlspec, knob_controlspec)
+    params:add_control(self.id.."_steps", "Steps", sequence_controlspec)
+    params:add_control(self.id.."_knob", "Knob", knob_controlspec)
+
+    self.dials.steps:set_value(sequence_controlspec.default)
+    self.dials.knob:set_value(get_linexp_value(knob_controlspec.default))
+end
+
+function Machine:get_steps()
+    return params:get(self.id..'_steps')
+end
+
+function Machine:set_steps_delta(delta)
+    params:delta(self.id..'_steps', delta)
+
+    local new_value = params:get(self.id..'_steps')
+    self.dials.steps:set_value(new_value)
+    self.dials.steps:set_marker_position(1, new_value)
+end
+
+function Machine:get_knob()
+    return get_linexp_value(params:get(self.id..'_knob'))
+end
+
+function Machine:set_knob_delta(delta)
+    params:delta(self.id..'_knob', delta)
+
+    local new_value = get_linexp_value(params:get(self.id..'_knob'))
+    self.dials.knob:set_value(new_value)
+    self.dials.knob:set_marker_position(1, new_value)
+end
+
+function Machine:toggle_running()
+    self.running = not self.running
+end
+
+function Machine:init_sequence()
+    self.sequence = {}
+    for i=1,self.sequence_length do
+        self.sequence[i] = math.random()
+    end
+end
+
+function Machine:randomize_current_step()
+    self.sequence[self.position] = math.random()
+end
+
+function Machine:update_sequence_and_get_value()
+    self:mutate_sequence()
+    local current_value = self.sequence[self.position]
+    if self.running then
+        self:move_to_next_position()
+    end
+    return current_value
+end
+
+function Machine:move_to_next_position()
+    self.position = util.wrap(self.position + 1, 1, self:get_steps())
+end
+
+function Machine:mutate_sequence()
+    local knob = self:get_knob()
+    local steps = self:get_steps()
+    if knob < 50 then
+        local probability = 50 - knob
+        if math.random(50) <= probability then
+            self.sequence[self.position] = math.random()
+        end
+    elseif knob > 50 then
+        local probability = knob - 50
+        if math.random(50) <= probability then
+            -- Find other position to swap value with
+            local other_position = self.position
+            while other_position == self.position do
+                other_position = math.random(steps)
+            end
+
+            -- Swap value with other position
+            local tmp = self.sequence[other_position]
+            self.sequence[other_position] = self.sequence[self.position]
+            self.sequence[self.position] = tmp
+        end
+    end
+end
+
+function Machine:draw_sequence(x, y, scale)
+    local index = self.position
+    local steps = self:get_steps()
+    for i=0,math.min(steps - 1, 7) do
+        index = util.wrap(self.position + i, 1, steps)
+        screen.level(math.floor(self.sequence[index] * 15 + 1))
+        screen.rect(x + i * 8, y - scale, scale, scale)
+        screen.fill()
+    end
+end
+
+function Machine:draw_dials()
+    self.dials.steps:redraw()
+    self.dials.knob:redraw()
+    screen.move(53, 20)
+    screen.text('Steps')
+    screen.move(100, 20)
+    screen.text('Knob')
+end
+
+function Machine:set_dials_active(state)
+    self.dials.steps.active = state
+    self.dials.knob.active = state
+end
+
+function get_linexp_value(x)
+    x = x - 50
+    if (x > 0) then
+        x = util.linexp(0, 50, 1, 51, x) - 1
+    else
+        x = util.linexp(-50, 0, -51, -1, x) + 1
+    end
+    return util.round(x+50, 0.5)
+end
+
+return Machine
--- turing_machines.lua
+++ turing_machines.lua
@@ -9,24 +9,20 @@
 
 -- TODO Add synth params control
 
--- TODO Refactor using class for sequences
-
 mu = require "musicutil"
 ui = require 'ui'
 util = require 'util'
 fm = require 'formatters'
 
-turing_params = {"note", "velocity", "ratcheting", "duration", "probability"}
-sequences = {}
-positions = {}
-paused = {}
-dials = {}
+Machine = include('lib/machine')
+
+machines = {}
+current_machine = nil
 
 engine.name = 'PolyPerc'
 
 scale_notes = {}
 running = true
-current_page = 1
 alt = false
 
 ratcheting_options = {1, 2, 3, 4, 6, 8, 12, 16, 24}
@@ -38,15 +34,7 @@ for i=1,#durations_labels do durations[durations_labels[i]] = durations_values e
 ratcheting_metro = metro.init()
 
 function init()
-    for i=1,#turing_params do
-        init_sequence(turing_params[i])
-        positions[turing_params[i]] = 1
-        dials[turing_params[i]] = {
-            steps=ui.Dial.new(55, 28, 22, 8, 1, 16, 1, 1, {8}),
-            knob=ui.Dial.new(100, 28, 22, 50, 0, 100, 1, 50, {50})
-        }
-        paused[turing_params[i]] = false
-    end
+    init_machines()
     set_params()
     build_scale()
 
@@ -55,14 +43,26 @@ function init()
 
     norns.enc.sens(1, 12)
 
-    for i=1,#turing_params do
-        dials[turing_params[i]].steps:set_value(params:get(turing_params[i]..'_steps'))
-        dials[turing_params[i]].knob:set_value(get_linexp_knob_value(params:get(turing_params[i]..'_knob')))
+    ratcheting_metro.event = play_next_note
+    clock.run(update)
+end
+
+function init_machines()
+    local machine_ids = {"notes", "velocity", "ratcheting", "duration", "probability"}
+    local machine_labels = {"Notes", "Velocity", "Ratcheting", "Duration", "Probability"}
+    local previous = nil
+    local machine = nil
+    for i=1,#machine_ids do
+        machine = Machine.new(machine_ids[i], machine_labels[i])
+        if previous then
+            previous.next = machine
+            machine.previous = previous
+        end
+        previous = machine
+        machines[machine_ids[i]] = machine
     end
 
-    ratcheting_metro.event = play_next_note
-
-    clock.run(update)
+    current_machine = machines['notes']
 end
 
 function set_params()
@@ -76,9 +76,10 @@ function set_params()
     local cs_SEQL = controlspec.new(1,16,'lin',1,8,'')
     local cs_KNOB = controlspec.new(0,100,'lin',1,50,'%')
 
-    params:add_group("Notes", 5)
-    params:add_control("note_steps", "Steps", cs_SEQL)
-    params:add_control("note_knob", "Knob", cs_KNOB)
+    -- Notes
+    local machine = machines['notes']
+    params:add_group(machine.label, 5)
+    machine:add_params(cs_SEQL, cs_KNOB)
     local cs_NOTE = controlspec.MIDINOTE:copy();
     cs_NOTE.default = 48;
     cs_NOTE.step = 1;
@@ -90,9 +91,10 @@ function set_params()
     params:add{type="control", id="octave_range", name="Octave range", controlspec=cs_OCTR, formatter=fm.round(1),
     action=function(x) build_scale() end}
 
-    params:add_group("Velocity", 4)
-    params:add_control("velocity_steps", "Steps", cs_SEQL)
-    params:add_control("velocity_knob", "Knob", cs_KNOB)
+    -- Notes
+    machine = machines['velocity']
+    params:add_group(machine.label, 4)
+    machine:add_params(cs_SEQL, cs_KNOB)
     local cs_V = controlspec.MIDIVELOCITY:copy();
     cs_V.default = 30;
     params:add_control("velocity_min", "Min", cs_V, fm.round(1))
@@ -100,24 +102,27 @@ function set_params()
     cs_V.default = 100;
     params:add_control("velocity_max", "Max", cs_V, fm.round(1))
 
-    params:add_group("Ratcheting", 4)
-    params:add_control("ratcheting_steps", "Steps", cs_SEQL)
-    params:add_control("ratcheting_knob", "Knob", cs_KNOB)
+    -- Ratcheting
+    machine = machines['ratcheting']
+    params:add_group(machine.label, 4)
+    machine:add_params(cs_SEQL, cs_KNOB)
     params:add{type="option", id="ratcheting_min", name="Min", options=ratcheting_options, default=1} -- ratcheting_options[1]
     params:add{type="option", id="ratcheting_max", name="Max", options=ratcheting_options, default=4} -- ratcheting_options[4]
 
-    params:add_group("Duration", 4)
-    params:add_control("duration_steps", "Steps", cs_SEQL)
-    params:add_control("duration_knob", "Knob", cs_KNOB)
+    -- Duration
+    machine = machines['duration']
+    params:add_group(machine.label, 4)
+    machine:add_params(cs_SEQL, cs_KNOB)
     local cs_DUR = controlspec.new(1,#durations_labels,'lin',1,1)
     params:add{type="control", id="duration_min", name="Min", controlspec=cs_DUR, formatter=function(param) return durations_labels[param:get()] end}
     cs_DUR = cs_DUR:copy()
     cs_DUR.default = 3
     params:add{type="control", id="duration_max", name="Max", controlspec=cs_DUR, formatter=function(param) return durations_labels[param:get()] end}
 
-    params:add_group("Probability", 4)
-    params:add_control("probability_steps", "Steps", cs_SEQL)
-    params:add_control("probability_knob", "Knob", cs_KNOB)
+    -- Probability
+    machine = machines['probability']
+    params:add_group(machine.label, 4)
+    machine:add_params(cs_SEQL, cs_KNOB)
     local cs_PROB = controlspec.AMP
     cs_PROB.default = 0.75
     params:add_control("probability_min", "Min", cs_PROB)
@@ -128,62 +133,17 @@ function set_params()
     params:default()
 end
 
-function get_linexp_knob_value(x)
-    x = x - 50
-    if (x > 0) then
-        x = util.linexp(0, 50, 1, 51, x) - 1
-    else
-        x = util.linexp(-50, 0, -51, -1, x) + 1
-    end
-    return util.round(x+50, 0.5)
-end
-
 function build_scale()
-    scale_notes = mu.generate_scale(params:get("root_note"), params:get("scale"), params:get("octave_range"))
-end
-
-function init_sequence(param_name)
-    sequences[param_name] = {}
-    for i=1,16 do
-        sequences[param_name][i] = math.random()
-    end
-end
-
-function update_sequence(param_name)
-    mutate_sequence(param_name)
-    local current_value = sequences[param_name][positions[param_name]]
-    if not paused[param_name] then move_to_next_position(param_name) end
-    return current_value
-end
-
-function move_to_next_position(param_name)
-    positions[param_name] = util.wrap(positions[param_name] + 1, 1, params:get(param_name..'_steps'))
-end
-
-function mutate_sequence(param_name)
-    local k = params:get(param_name.."_knob")
-    local cp = positions[param_name]
-    if k < 50 then
-        local p = 50 - k
-        if math.random(50) <= p then sequences[param_name][cp] = math.random() end
-    elseif k > 50 then
-        local p = k - 50
-        local sl = params:get(param_name..'_steps')
-        if math.random(50) <= p then
-            local other = cp
-            while other == cp do other = math.random(sl) end
-            local tmp = sequences[param_name][other]
-            sequences[param_name][other] = sequences[param_name][cp]
-            sequences[param_name][cp] = tmp
-        end
-    end
+    scale_notes = mu.generate_scale(
+        params:get("root_note"), params:get("scale"), params:get("octave_range"))
 end
 
 function update()
     while true do
         clock.sync(1)
         if running then
-            local ratcheting = update_sequence("ratcheting")
+            local machine = machines['ratcheting']
+            local ratcheting = machine:update_sequence_and_get_value()
             ratcheting = math.floor(ratcheting * math.abs(params:get("ratcheting_max") - params:get("ratcheting_min")) + math.min(params:get("ratcheting_max"), params:get("ratcheting_min")) + 0.5) -- + 0.5 to round instead of floor
             play_next_note()
             if ratcheting > 1 then
@@ -194,21 +154,25 @@ function update()
 end
 
 function play_next_note()
-    local probability = update_sequence("probability")
+    local machine = machines['probability']
+    local probability = machine:update_sequence_and_get_value()
     probability = probability * math.abs(params:get("probability_max") - params:get("probability_min")) + math.min(params:get("probability_max"), params:get("probability_min"))
     local should_play = math.random() <= probability
 
-    local duration_index = update_sequence("duration")
+    machine = machines['duration']
+    local duration_index = machine:update_sequence_and_get_value()
     duration_index = math.floor(duration_index * math.abs(params:get("duration_max") - params:get("duration_min")) + math.min(params:get("duration_max"), params:get("duration_min")) + 0.5)
     local duration = durations_values[duration_index]
     if should_play then engine.release(clock:get_beat_sec() * duration) end
 
-    local next = update_sequence("velocity")
+    machine = machines['velocity']
+    local next = machine:update_sequence_and_get_value()
     next = next * math.abs(params:get("velocity_max") - params:get("velocity_min")) + math.min(params:get("velocity_max"), params:get("velocity_min"))
     next = next / 127
     if should_play then engine.amp(next) end
 
-    next = update_sequence("note")
+    machine = machines['notes']
+    next = machine:update_sequence_and_get_value()
     next = math.floor(next * params:get("octave_range") * 12 + params:get("root_note"))
     next = mu.snap_note_to_array(next, scale_notes)
     if should_play then engine.hz(mu.note_num_to_freq(next)) end
@@ -225,8 +189,8 @@ function stop()
 end
 
 function reset()
-    for i=1,#turing_params do
-        positions[turing_params[i]]=1
+    for _, machine in pairs(machines) do
+        machine.position = 1
     end
 end
 
@@ -284,48 +248,40 @@ end
 
 function enc(index, delta)
     if index==1 then
-        current_page = util.clamp(current_page + delta, 1, #turing_params)
+        if delta < 0 and current_machine.previous then
+            current_machine = current_machine.previous
+        elseif delta > 0 and current_machine.next then
+            current_machine = current_machine.next
+        end
     end
 
     if not alt then
         if index==2 then
-            params:delta(turing_params[current_page]..'_steps', delta)
-            local v = params:get(turing_params[current_page]..'_steps')
-            dials[turing_params[current_page]].steps:set_value(v)
-            dials[turing_params[current_page]].steps:set_marker_position(1, v)
+            current_machine:set_steps_delta(delta)
         elseif index==3 then
-            params:delta(turing_params[current_page]..'_knob', delta)
-            local v = get_linexp_knob_value(params:get(turing_params[current_page]..'_knob'))
-            dials[turing_params[current_page]].knob:set_value(v)
-            dials[turing_params[current_page]].knob:set_marker_position(1, v)
+            current_machine:set_knob_delta(delta)
         end
     else
-        if current_page == 1 then control_note_page(index, delta)
-        elseif current_page == 2 then control_velocity_page(index, delta)
-        elseif current_page == 3 then control_ratcheting_page(index, delta)
-        elseif current_page == 4 then control_duration_page(index, delta)
-        elseif current_page == 5 then control_probability_page(index, delta) end
+        if current_machine.id == 'notes' then control_note_page(index, delta)
+        elseif current_machine.id == 'velocity' then control_velocity_page(index, delta)
+        elseif current_machine.id == 'ratcheting' then control_ratcheting_page(index, delta)
+        elseif current_machine.id == 'duration' then control_duration_page(index, delta)
+        elseif current_machine.id == 'probability' then control_probability_page(index, delta) end
     end
 
     redraw()
 end
 
 function key(index, state)
-    local current_param = turing_params[current_page]
     if index == 1 then
         alt = state == 1
-        dials[current_param].steps.active = not alt
-        dials[current_param].knob.active = not alt
+        current_machine:set_dials_active(not alt)
     elseif index == 2 and state == 1 then
-        -- move to next step
-        if alt then move_to_next_position(current_param)
-            -- pause sequence
-        else paused[current_param] = not paused[current_param] end
+        if alt then current_machine:move_to_next_position()
+        else current_machine:toggle_running() end
     elseif index == 3 and state == 1 then
-        -- randomize current step
-        if alt then sequences[current_param][positions[current_param]] = math.random()
-            -- randomize whole sequence
-        else init_sequence(current_param) end
+        if alt then current_machine:randomize_current_step()
+        else current_machine:init_sequence() end
     end
     redraw()
 end
@@ -402,44 +358,23 @@ function draw_probability_page()
     screen.text(params:string('probability_max'))
 end
 
-function draw_seq()
-    local current_param = turing_params[current_page]
-    local s = sequences[current_param]
-    local sl = params:get(current_param..'_steps')
-    local start_index = positions[current_param]
-    local index = start_index
-    for i=0,math.min(sl - 1, 7) do
-        index = util.wrap(start_index + i, 1, sl)
-        screen.level(math.floor(s[index] * 15 + 1))
-        screen.rect(60 + i * 8, text_positions.title - 5, 5, 5)
-        screen.fill()
-    end
-end
-
 function redraw()
-    local current_param = turing_params[current_page]
-
     screen.clear()
     screen.fill()
 
-    dials[current_param].steps:redraw()
-    dials[current_param].knob:redraw()
-    screen.move(53, 20)
-    screen.text('Steps')
-    screen.move(100, 20)
-    screen.text('Knob')
+    current_machine:draw_dials()
 
     screen.level(1)
     screen.move(0, text_positions.title)
-    screen.text(string.upper(current_param))
+    screen.text(string.upper(current_machine.label))
 
-    if current_page == 1 then draw_note_page()
-    elseif current_page == 2 then draw_velocity_page()
-    elseif current_page == 3 then draw_ratcheting_page()
-    elseif current_page == 4 then draw_duration_page()
-    elseif current_page == 5 then draw_probability_page() end
+    if current_machine.id == 'notes' then draw_note_page()
+    elseif current_machine.id == 'velocity' then draw_velocity_page()
+    elseif current_machine.id == 'ratcheting' then draw_ratcheting_page()
+    elseif current_machine.id == 'duration' then draw_duration_page()
+    elseif current_machine.id == 'probability' then draw_probability_page() end
 
-    draw_seq()
+    current_machine:draw_sequence(60, text_positions.title, 5)
 
     screen.update()
 end
